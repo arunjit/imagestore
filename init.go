42d19890b5178e0faecf2aafc57995e25ca2506c
Arunjit Singh <opensrc@ajsd.in>
2013-07-29 01:48:16 +0530

    add HTTPError and serve() wrapper

--- init.go
+++ init.go
@@ -5,8 +5,31 @@ import (
 	"net/http"
 )
 
+type HTTPError struct {
+	err    error
+	status int
+}
+
+func (e HTTPError) Error() string {
+	return fmt.Sprintf("[%d] %s", e.status, e.err.Error())
+}
+
+func serve(f func(w http.ResponseWriter, r *http.Request) error) http.HandlerFunc {
+	return func(w http.ResponseWriter, r *http.Request) {
+		err := f(w, r)
+		if err != nil {
+			msg := err.Error()
+			code := http.StatusInternalServerError
+			if httpErr, ok := err.(HTTPError); ok {
+				code = httpErr.status
+			}
+			http.Error(w, msg, code)
+		}
+	}
+}
+
 func init() {
-	http.HandleFunc("/upload", upload)
+	http.HandleFunc("/upload", serve(upload))
 	http.HandleFunc("/", index)
 }
 
--- store.go
+++ store.go
@@ -2,7 +2,7 @@ package imagestore
 
 import (
 	"encoding/json"
-	"fmt"
+	"errors"
 	"io"
 	"net/http"
 
@@ -20,20 +20,13 @@ type Image struct {
 	URL     string            `datastore:"u" json:"url,omitempty"`
 }
 
-// TODO(arunjit): Instead of panic-recover, writer a simple wrapper so that
-// the handler can return an error.
-
-func recoverHTTP(w http.ResponseWriter) {
-	if r := recover(); r != nil {
-		http.Error(w, fmt.Sprintf("ERROR: %v\n", r), 500) // TODO(arunjit): status
-	}
-}
-
-func upload(w http.ResponseWriter, r *http.Request) {
-	defer recoverHTTP(w)
+func upload(w http.ResponseWriter, r *http.Request) error {
 	mime := r.Header.Get("Content-Type")
 	if mime == "" {
-		panic("Missing image Content-Type [header]")
+		return HTTPError{
+			errors.New("Missing image Content-Type [header]"),
+			http.StatusBadRequest,
+		}
 	}
 
 	c := appstats.NewContext(r)
@@ -41,7 +34,7 @@ func upload(w http.ResponseWriter, r *http.Request) {
 
 	bw, err := blobstore.Create(c, mime)
 	if err != nil {
-		panic("Couldn't create blobstore.Writer: " + err.Error())
+		return errors.New("Couldn't create blobstore.Writer: " + err.Error())
 	}
 	io.Copy(bw, r.Body)
 	r.Body.Close()
@@ -49,26 +42,27 @@ func upload(w http.ResponseWriter, r *http.Request) {
 
 	bkey, err := bw.Key()
 	if err != nil {
-		panic("Couldn't get key for blob: " + err.Error())
+		return errors.New("Couldn't get key for blob: " + err.Error())
 	}
 
 	// opts := &image.ServingURLOptions{Secure:true}
 	url, err := image.ServingURL(c, bkey, nil)
 	if err != nil {
-		panic("Couldn't create serving URL for blob: " + err.Error())
+		return errors.New("Couldn't create serving URL for blob: " + err.Error())
 	}
 
 	img := &Image{BlobKey: bkey, URL: url.String()}
 
 	key, err := datastore.Put(c, datastore.NewIncompleteKey(c, "IMG", nil), img)
 	if err != nil {
-		panic("Couldn't save to datastore: " + err.Error())
+		return errors.New("Couldn't save to datastore: " + err.Error())
 	}
 
 	img.ID = key.IntID()
 
 	err = json.NewEncoder(w).Encode(img)
 	if err != nil {
-		panic("Couldn't create JSON: " + err.Error())
+		return errors.New("Couldn't create JSON: " + err.Error())
 	}
+	return nil
 }
